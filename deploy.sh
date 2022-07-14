b5d676a35bebcab6b53eab7e745dbf10dace369a
yakovdmy <Dmytro.Yakovenko@externals.adidas.com>
2022-07-14 12:50:07 +0300

    adjust travis.yml, add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t dyakodocker/multi-client:latest -t dyakodocker/multi-client:$SHA -f ./client/Dockerfile ./multi-client
+docker build -t dyakodocker/multi-server:latest -t dyakodocker/multi-server:$SHA -f ./server/Dockerfile ./multi-server
+docker build -t dyakodocker/multi-worker:latest -t dyakodocker/multi-worker:$SHA -f ./worker/Dockerfile ./multi-worker
+
+docker push dyakodocker/multi-client:latest
+docker push dyakodocker/multi-server:latest
+docker push dyakodocker/multi-worker:latest
+
+docker push dyakodocker/multi-client:$SHA
+docker push dyakodocker/multi-server:$SHA
+docker push dyakodocker/multi-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/client-deployment client=dyakodocker/multi-client:$SHA
+kubectl set image deployments/server-deployment server=dyakodocker/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=dyakodocker/multi-worker:$SHA
